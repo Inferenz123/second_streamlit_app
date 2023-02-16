16307c6f3d73f135b7b1e6233d17761996ad0e19
Inferenz123 <125567938+Inferenz123@users.noreply.github.com>
2023-02-16 23:01:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title( Breakfast Menu 
-                omega 3 and blueberry oatmeal
+streamlit.title( Breakfast menu ,omega 3 and blueberry oatmeal
                 kale, spinach and smoothie
                 hard-boiled free-range egg)
