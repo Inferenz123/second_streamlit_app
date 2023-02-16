ca6b7330d0cfbd432bac87a9b0b743cf6d9a1ccb
Inferenz123 <125567938+Inferenz123@users.noreply.github.com>
2023-02-16 23:12:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My Mom New Healthy Diner')
 
 streamlit.header( 'Breakfast fav')
-streamlit.text( ':) omega 3 and blueberry oatmeal')
+streamlit.text( ' omega 3 and blueberry oatmeal')
 streamlit.text('kale, spinach and smoothie')
 streamlit.text('hard-boiled free-range egg')
 
